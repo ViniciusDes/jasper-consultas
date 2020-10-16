735c439b56b1a56a34cc35302837c2b134b2737c
Douglas Gomes <douglasvieira17@gmail.com>
2020-10-15 21:22:28 -0300

    Update programacao_ferias.sql

--- programacao_ferias.sql
+++ programacao_ferias.sql
@@ -1,37 +1,37 @@
 SELECT DISTINCT *
-FROM (SELECT RF_FILIAL                                                                        CODIGO_FILIAL,
-             APS_DESC_FILIAL                                                                  FILIAL,
-             RA_CC                                                                            CODIGO_CENTRO_CUSTO,
-             CTT_DESC01                                                                       CENTRO_CUSTO,
-             RF_MAT                                                                           MATRICULA,
-             RA_NOME                                                                          NOME,
-             CONVERT(VARCHAR, CONVERT(DATE, RA_ADMISSA), 103)                                 ADMISSAO,
-             CONVERT(VARCHAR, CONVERT(DATE, RF_DATABAS), 103)                                 BASE,
+FROM (SELECT RF_FILIAL                                                                               CODIGO_FILIAL,
+             APS_DESC_FILIAL                                                                         FILIAL,
+             RA_CC                                                                                   CODIGO_CENTRO_CUSTO,
+             CTT_DESC01                                                                              CENTRO_CUSTO,
+             RF_MAT                                                                                  MATRICULA,
+             RA_NOME                                                                                 NOME,
+             CONVERT(VARCHAR, CONVERT(DATE, RA_ADMISSA), 103)                                        ADMISSAO,
+             CONVERT(VARCHAR, CONVERT(DATE, RF_DATABAS), 103)                                        BASE,
              CONVERT(VARCHAR, CONVERT(DATE, RF_DATABAS), 103) + ' - ' +
-             CONVERT(VARCHAR, CONVERT(DATE, RF_DATAFIM), 103)                                 PERIODO_AQUISITIVO,
+             CONVERT(VARCHAR, CONVERT(DATE, RF_DATAFIM), 103)                                        PERIODO_AQUISITIVO,
              CONVERT(VARCHAR, CONVERT(DATE, DATEADD(DAY, 30,
                                                     SUBSTRING(RF_DATAFIM, 5, 2) + '/' + SUBSTRING(RF_DATAFIM, 7, 2) +
-                                                    '/' + SUBSTRING(RF_DATAFIM, 1, 4))), 103) DATA_LIMITE_IDEAL,
+                                                    '/' + SUBSTRING(RF_DATAFIM, 1, 4))), 103)        DATA_LIMITE_IDEAL,
              CONVERT(VARCHAR, CONVERT(DATE, DATEADD(DAY, 320,
                                                     SUBSTRING(RF_DATAFIM, 5, 2) + '/' + SUBSTRING(RF_DATAFIM, 7, 2) +
-                                                    '/' + SUBSTRING(RF_DATAFIM, 1, 4))), 103) DATA_LIMITE_MAX,
-             RF_DFERVAT - SUM(RH_DFERIAS)                                                     VENCIDAS,
+                                                    '/' + SUBSTRING(RF_DATAFIM, 1, 4))), 103)        DATA_LIMITE_MAX,
+             RF_DFERVAT - ISNULL(SUM(RH_DFERIAS), 0)                                                 VENCIDAS,
              isnull(CASE
                         WHEN RF_DFALVAT >= 6 AND RF_DFALVAT <= 14 THEN 6
                         WHEN RF_DFALVAT >= 15 AND RF_DFALVAT <= 23 THEN 12
                         WHEN RF_DFALVAT >= 24 AND RF_DFALVAT <= 32 THEN 18
-                        WHEN RF_DFALVAT > 32 THEN 30 END, 0)                                  D_ABAT,
+                        WHEN RF_DFALVAT > 32 THEN 30 END, 0)                                         D_ABAT,
              IIF(RF_DFERVAT <> 0, 0, datediff(month,
                                               SUBSTRING(RF_DATABAS, 5, 2) + '/' + SUBSTRING(RF_DATABAS, 7, 2) + '/' +
                                               SUBSTRING(RF_DATABAS, 1, 4),
                                               SUBSTRING(RCH_DTPAGO, 5, 2) + '/' + SUBSTRING(RCH_DTPAGO, 7, 2) + '/' +
-                                              SUBSTRING(RCH_DTPAGO, 1, 4)) * 2.5)             PROPORCIONAIS,
-             IIF(RF_DATAINI = '', '', dbo.stod(RF_DATAINI))                                   "1_PROG_FERIAS",
-             RF_DFEPRO1                                                                       D_1_PROG_FERIAS,
-             IIF(RF_DATINI2 = '', '', dbo.stod(RF_DATINI2))                                   "2_PROG_FERIAS",
-             RF_DFEPRO2                                                                       D_2_PROG_FERIAS,
-             IIF(RF_DATINI3 = '', '', dbo.stod(RF_DATINI3))                                   "3_PROG_FERIAS",
-             RF_DABPRO3                                                                       D_3_PROG_FERIAS,
+                                              SUBSTRING(RCH_DTPAGO, 1, 4)) * 2.5)                    PROPORCIONAIS,
+             IIF(RF_DATAINI = '', '', dbo.stod(RF_DATAINI))                                          "1_PROG_FERIAS",
+             RF_DFEPRO1                                                                              D_1_PROG_FERIAS,
+             IIF(RF_DATINI2 = '', '', dbo.stod(RF_DATINI2))                                          "2_PROG_FERIAS",
+             RF_DFEPRO2                                                                              D_2_PROG_FERIAS,
+             IIF(RF_DATINI3 = '', '', dbo.stod(RF_DATINI3))                                          "3_PROG_FERIAS",
+             RF_DABPRO3                                                                              D_3_PROG_FERIAS,
              RF_DFERVAT - isnull(CASE
                                      WHEN RF_DFALVAT >= 6 AND RF_DFALVAT <= 14 THEN 6
                                      WHEN RF_DFALVAT >= 15 AND RF_DFALVAT <= 23 THEN 12
@@ -42,7 +42,7 @@ FROM (SELECT RF_FILIAL                                                          
                   ELSE datediff(MONTH, SUBSTRING(RF_DATABAS, 5, 2) + '/' + SUBSTRING(RF_DATABAS, 7, 2) + '/' +
                                        SUBSTRING(RF_DATABAS, 1, 4),
                                 SUBSTRING(RCH_DTPAGO, 5, 2) + '/' + SUBSTRING(RCH_DTPAGO, 7, 2) + '/' +
-                                SUBSTRING(RCH_DTPAGO, 1, 4)) * 2.5 END) - SUM(RH_DFERIAS)     TOTAL
+                                SUBSTRING(RCH_DTPAGO, 1, 4)) * 2.5 END) - ISNULL(SUM(RH_DFERIAS), 0) TOTAL
       FROM SRF$P!{CODIGO_EMPRESA} SRF
                JOIN API_SIGAMAT ON RF_FILIAL = APS_COD_FILIAL AND APS_COD_EMPRESA = '$P!{CODIGO_EMPRESA}'
                JOIN SRA$P!{CODIGO_EMPRESA} SRA ON RA_FILIAL = RF_FILIAL AND RA_MAT = RF_MAT AND RA_SITFOLH NOT IN ('D') AND
